179680f2e14e42129a064a538adf9c09b1f6174a
osha7 <ogroetz@gmail.com>
2020-10-11 12:32:31 -0600

    submit

--- src/components/Translator.js
+++ src/components/Translator.js
@@ -2,17 +2,45 @@ import React from 'react';
 
 class Translator extends React.Component {
 
+  // newArray = []
+  // componentDidMount = () => {
+  //   // debugger
+  //   const { translations } = this.props
+  //   console.log (translations)
+  //   translations.forEach((value, key) => {
+  //     console.log("componentDidMount", `${key}, ${value}`)
+  //     this.newArray.push([`${key}, ${value}`])
+  //   })
+  //   // debugger
+  //   console.log(this.newArray)
+  // }
+
+  state = {
+    input: '',
+    // output: ''
+  }
+
+  inputOnChange = (e) => {
+    this.setState({
+      input: e.target.value
+    }) 
+    // console.log(e.target.value)
+    // console.log(this.state.input)
+  }
+
   render() {
+    const { input } = this.state;
+    const { translations } = this.props;
     return (
       <React.Fragment>
         <div className="controls">
           <div className="input-container">
             <span>input:</span>
-            <input type="text" className="text-input" data-testid="text-input" />
+            <input onChange={this.inputOnChange} value={this.state.input} type="text" className="text-input" data-testid="text-input" />
           </div>
           <div className="input-container">
             <span>output:</span>
-            <input type="text" className="text-output" data-testid="text-output" readOnly />
+            <input value={translations.get(input) || ''} type="text" className="text-output" data-testid="text-output" readOnly />
           </div>
         </div>
       </React.Fragment>
